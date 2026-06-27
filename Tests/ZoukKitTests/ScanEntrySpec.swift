e19b79cbca22b8ba2eb7f161762ce2d2b2b35652
John Woodell <woodie@netpress.com>
2026-06-27 09:08:41 -0700

    Fix lint errors.

--- Tests/ZoukKitTests/ScanEntrySpec.swift
+++ Tests/ZoukKitTests/ScanEntrySpec.swift
@@ -9,7 +9,9 @@ final class ScanEntrySpec: QuickSpec {
             describe("decoding from server JSON") {
                 it("decodes the name, size, time, and url fields") {
                     let json = Data("""
-                    [{"name":"1779907271.pdf","size":7,"time":"2026-06-25T10:30:00-07:00","url":"/download/1779907271.pdf"}]
+                    [{"name":"1779907271.pdf","size":7,
+                      "time":"2026-06-25T10:30:00-07:00",
+                      "url":"/download/1779907271.pdf"}]
                     """.utf8)
 
                     let scans = try JSONDecoder().decode([ScanEntry].self, from: json)
@@ -24,7 +26,8 @@ final class ScanEntrySpec: QuickSpec {
 
             describe("#formattedSize") {
                 it("is human readable for a multi-megabyte file") {
-                    let scan = ScanEntry(name: "a.pdf", size: 1_500_000, time: "2026-06-25T10:30:00-07:00", url: "/download/a.pdf")
+                    let scan = ScanEntry(name: "a.pdf", size: 1_500_000,
+                    time: "2026-06-25T10:30:00-07:00", url: "/download/a.pdf")
                     expect(scan.formattedSize).to(contain("MB"))
                 }
             }
@@ -37,14 +40,16 @@ final class ScanEntrySpec: QuickSpec {
 
                 context("with a valid timestamp") {
                     it("is non-nil") {
-                        let scan = ScanEntry(name: "a.pdf", size: 7, time: "2026-06-25T10:30:00-07:00", url: "/download/a.pdf")
+                        let scan = ScanEntry(name: "a.pdf", size: 7,
+                        time: "2026-06-25T10:30:00-07:00", url: "/download/a.pdf")
                         expect(scan.formattedDate).toNot(beNil())
                     }
                 }
 
                 context("with an unparsable timestamp") {
                     it("is nil, along with downloadedAt") {
-                        let scan = ScanEntry(name: "a.pdf", size: 7, time: "not-a-date", url: "/download/a.pdf")
+                        let scan = ScanEntry(name: "a.pdf", size: 7,
+                        time: "not-a-date", url: "/download/a.pdf")
                         expect(scan.downloadedAt).to(beNil())
                         expect(scan.formattedDate).to(beNil())
                     }
